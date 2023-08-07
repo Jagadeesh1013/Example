e964220ed915510b4afa9940c1bd68ca7397a61a
Jagadeesh <jagadeeshkanchana105@gmail.com>
2023-08-07 11:36:48 +0530

    First Commit...!!!

---
+++ src/com/jagadeesh/example/Demo1.java
@@ -0,0 +1,17 @@
+package com.jagadeesh.example;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		String str = "Hungry";
+		
+		if(str == "Hungry") {
+			System.out.println("I am so Hungry...");
+		}
+		else {
+			System.out.println("I am not Hungry...");
+		}
+	}
+
+}
